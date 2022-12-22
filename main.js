2b24e1aed297ab5bcc3ea8e37a9c2223965cb2f6
OG <og.exo696@gmail.com>
2022-12-22 23:49:13 +0500

    new project

---
+++ main.js
@@ -0,0 +1,24 @@
+var myName = 'Maxmud'
+
+
+console.log(myName);
+
+
+
+console.log(prompt('Ismingizzi kiriting'));
+console.log(prompt('Familyangizzi kiriting'));
+console.log(prompt('Yoshingizzi kiriting'));
+var str =+prompt('6+3=?');
+alert(' Foydalanuvchi javobingiz '+str+' togri javob ' + (6+3));
+console.log(str);
+
+
+var str=prompt('Nechta Qovun olasiz - Qovun narxi 3.000 ');
+console.log(str);
+alert('Qovun: Sizdan'+ ((str)*3000));
+console.log(str);
+var str=prompt('Nechta Tarvuz olasiz - Tarvuz narxi 2.000');
+console.log(str);
+alert('Tarvuz: Sizdan'+ ((str)*2000));
+alert('Xaridingiz uchun raxmat)');
+console.log(str);
